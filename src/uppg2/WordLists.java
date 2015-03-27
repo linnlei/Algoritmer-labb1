9bd28e99ba138ca214037ce457dad640514d9ccd
linnlei <linn.leiulfsrud@gmail.com>
2015-03-28 00:23:05 +0100

    Hittat felen
    Har försökt kommentera så förklarande jag kan :)

--- src/uppg2/WordLists.java
+++ src/uppg2/WordLists.java
@@ -21,36 +21,19 @@ public class WordLists {
 	private TreeMap<String, Integer> wordMap;
 	private TreeMap<String, Integer> wordMap2;
 	private TreeMap<Integer, TreeSet<String>> freqMap;
-	private TreeMap<Integer, TreeSet<String>> freqMap2;
-	private TreeSet<String> reverseSet;
 	private TreeMap<String, String> reverseMap;
-	private StringComparator stringComp;
-	private StringComparatorDescending stringCompDesc;
-	private StringComparatorDescending stringCompDesc2;
 	private String fileName;
 
-	public WordLists(String inputFileName) {
+	public WordLists(String inputFileName) throws FileNotFoundException {
 		
-	    try {
-			in = new BufferedReader(new FileReader(inputFileName));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-	   
+		in = new BufferedReader(new FileReader(inputFileName));
 	    fileName = inputFileName;
-	    /*try {
-			in.mark(0);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
+	    
 		wordMap = new TreeMap<String, Integer>();
 		wordMap2 = new TreeMap<String, Integer>();
-		freqMap = new TreeMap<Integer, TreeSet<String>>(stringCompDesc);
-		freqMap2 = new TreeMap<Integer, TreeSet<String>>(stringCompDesc2);
+		freqMap = new TreeMap<Integer, TreeSet<String>>(new StringComparatorDescending());
+		reverseMap = new TreeMap<String, String>(new StringComparator());
 		
-		reverseSet = new TreeSet<String>();
-		reverseMap = new TreeMap<String, String>(stringComp);
 	}
 	
 	private boolean isPunctuationChar(char c) {
@@ -96,25 +79,22 @@ public class WordLists {
 	    	reversedArray[wordArray.length - x - 1] = wordArray[x];
 	    }
 	    
-	    //String hej2 = new String(wordArray);
 	    reversed = new String(reversedArray);
-	    //System.out.println("" + hej2);
-	    //System.out.println("" + hej3);
 		return reversed;
 	}
 	
+	
 	private void computeWordFrequencies() {
-		
 		String word = "";
 		try{
-			while( (word = getWord()) != null){
-				if (wordMap.containsKey(word) == true ){
+			while( (word = getWord()) != null){	
+				if (wordMap.containsKey(word) == true ){	//Om ordet redan finns i mapen
 					int value;
-					value = wordMap.get(word) + 1 ;
-					wordMap.put(word, value);
+					value = wordMap.get(word) + 1 ;			//Hämta ordets frekvens, lägg till 1
+					wordMap.put(word, value);				//Lägg in uppdaterade värdet i mapen
 				}
-				else{	//Första gången ett ord dyker upp
-					wordMap.put(word, 1);
+				else{										//Första gången ett ord dyker upp
+					wordMap.put(word, 1);					//Lägg till ordet i mapen och sätt ordets frekvens till 1
 				}
 			}
 		}
@@ -122,8 +102,7 @@ public class WordLists {
 			e.printStackTrace();
 		}
 		
-		//System.out.println("Java: " + wordMap.get("java") );
-		try {
+		try {												//För att skriva till textfil
 			BufferedWriter out = new BufferedWriter(new FileWriter("alfaSorted.txt"));
 			for(String s : wordMap.keySet()){
 				out.write(s + "      ");
@@ -137,104 +116,34 @@ public class WordLists {
 		}
 	}
 	
-	//HÄR SKRIVER VI
-	private void computeFrequencyMap() {
+	
+	private void computeFrequencyMap() throws FileNotFoundException {
 		
-
-		try {
-			in = new BufferedReader(new FileReader(fileName));
-		} catch (FileNotFoundException e2) {
-			e2.printStackTrace();
-		}
-		
+		in = new BufferedReader(new FileReader(fileName));	//Starta om från början på textfilen
 		
 		String word = "";
 		try{
 			while( (word = getWord()) != null){
-
-				//****************
-				//wordMap = new TreeMap<String, Integer>();
-				//freqMap = new TreeMap<Integer, TreeSet<String>>(stringCompDesc);
-				//****************
-
-				if (wordMap2.containsKey(word) == true ){
+				
+				if (wordMap2.containsKey(word) == true ){	//Om ordet redan finns i mapen
 					int value;
-					value = wordMap2.get(word) + 1 ;
-					wordMap2.put(word, value);
+					value = wordMap2.get(word) + 1 ;		//Hämta antal gånger ordet räknats hittills, lägg till 1
+					wordMap2.put(word, value);				//Lägg in uppdaterade värdet i mapen
 				}
-				else{	//Första gången ett ord dyker upp
+				else{										//Första gången ett ord dyker upp
 					wordMap2.put(word, 1);
 				}
-
-				for(String s : wordMap2.keySet()){
-					System.out.println("" + s);
-				}
 				
-				for(String s : wordMap2.keySet()){
-					TreeSet<String> treeSet = new TreeSet<String>();
-					
-					if(freqMap2.get(wordMap2.get(word)) != null)
-						treeSet = freqMap2.get(wordMap2.get(word));
-					
-					treeSet.add(word);
-					freqMap2.put(wordMap2.get(word), treeSet);
-					
-					//System.out.println("" + s);
-					//System.out.println("" + wordMap2.get(word));
-				}
-				/*
-				for(Integer i : wordMap.values()){
-					freqMap2.get(i).add(word);
-					freqMap2.put(i, freqMap2.get(i) );
-				}*/
+			}
+			
+			for(String s : wordMap2.keySet()){	//Använder wordMap som innehåller orden+frekvenser, för att skapa frekvensmapen
+				TreeSet<String> treeSet = new TreeSet<String>();
 				
-				/*
-				if (freqMap.values().toString().contains(word) == true ){
-					//System.out.println("HEJ IF!");
-					
-					for(Integer i :  freqMap.keySet() ){
-						TreeSet<String> treeSet = new TreeSet<String>();
-						treeSet = freqMap.get(i);
-						
-							for(String s : treeSet){
-								
-								if(s.equals(word)){
-									sumInt = Integer.sum(i.intValue(), 1);
-									//System.out.println("" + sumInt);
-									//Integer newValue = new Integer(sumInt);
-									
-									Integer newValue = new Integer(i + 1);
-									
-									TreeSet<String> treeSet2 = new TreeSet<String>();
-									if(freqMap.get(newValue) != null)
-										treeSet2 = freqMap.get(newValue);
-									treeSet2.add(word);
-									freqMap.put(newValue, treeSet2);
-									
-									//System.out.println("Summa: " + newValue);
-									
-									//freqMap.get(i).remove(word);
-									//treeSet.remove(word);
-									freqMap.put(i, treeSet);
-								}
-							}
-							freqMap.get(i).remove(word);	
-					}
-				}
-				else{	//Första gången ett ord dyker upp
-					//System.out.println("HEJ ELSE!");
-					Integer i = new Integer(1);
-					TreeSet<String> treeSet = new TreeSet<String>();
-					
-					if(freqMap.get(i) != null)
-						treeSet = freqMap.get(i);
-					
-					treeSet.add(word);
-					freqMap.put(1, treeSet);
-					//freqMap.put(1, freqMap.get(i).add(word));
-				}*/
+				if(freqMap.get(wordMap2.get(s)) != null)	//Om det redan finns ord under en frekvens
+					treeSet = freqMap.get(wordMap2.get(s));	//Så hämta treeSet'et med de orden
 				
-				
+				treeSet.add(s);								//Lägg till ordet i det nya/hämtade treeSet
+				freqMap.put(wordMap2.get(s), treeSet);		//Lägg in treeSet'et under rätt frekvens i frekvensmapen
 			}
 			
 			
@@ -245,14 +154,13 @@ public class WordLists {
 		
 		
 		
-		//System.out.println("Java: " + freqMap.get(1).toString() );
-		try {
+		try {												//För att skriva till textfil
 			TreeSet<String> emptySet = new TreeSet<String>();
 			BufferedWriter out = new BufferedWriter(new FileWriter("frequencySorted.txt"));
-			for(Integer number : freqMap2.keySet()){
-				if( !(freqMap2.get(number).equals(emptySet)) ){
+			for(Integer number : freqMap.keySet()){
+				if( !(freqMap.get(number).equals(emptySet)) ){
 					out.write(number + ":\n");
-					out.write("" + freqMap2.get(number).toString());
+					out.write("" + freqMap.get(number).toString());
 					out.write("\n");
 				}
 			}
@@ -263,34 +171,29 @@ public class WordLists {
 		}
 	}
 	
-//***********************************************************************************
-	private void computeBackwardsOrder() {
+	
+	private void computeBackwardsOrder() throws FileNotFoundException {
 		
-		try {
-			in = new BufferedReader(new FileReader(fileName));
-		} catch (FileNotFoundException e2) {
-			e2.printStackTrace();
-		}
-		
+		in = new BufferedReader(new FileReader(fileName));	//Starta om från början på textfilen
 		
 		String word = "";
 		String rword = "";
+		
 		try{
 			while( (word = getWord()) != null){
-				rword = reverse(word);
-				reverseMap.put(rword, word);
+				rword = reverse(word);				//Vänder på ordet genom reverse-metoden
+				reverseMap.put(rword, word);		//Lägger in bakvända ordet samt ordet
 			}
 		}
 		catch(IOException e){
 			e.printStackTrace();
 		}
-		
 
-		try {
+		try {										//För att skriva till textfil
 			BufferedWriter out = new BufferedWriter(new FileWriter("backwardsSorted.txt"));
-			for(String s : reverseMap.keySet()){
-				out.write(reverseMap.get(s));
-				out.write("\n");
+			for(String s : reverseMap.keySet()){	//Går igenom mapen, som är sorterad efter de bakvända orden
+				out.write(reverseMap.get(s));		//Skriver ut värdena i mapen, dvs de 'rättvända' orden
+				out.write("\n");				
 			}
 
 			out.close();
@@ -319,8 +222,8 @@ public class WordLists {
 	} 
 	
 	public class StringComparatorDescending implements Comparator<Integer> {
-		public int compare(Integer s1,Integer s2) {
-			if(s1.compareTo(s2) > 0)
+		public int compare(Integer s1,Integer s2) {	//Ger omvänd sortering, dvs returnerar omvänt vad "vanlig" komparator gör
+			if(s1.compareTo(s2) > 0)	
 				return -1;
 			else if(s1.compareTo(s2) < 0)
 				return 1;
@@ -330,22 +233,4 @@ public class WordLists {
 	} 
 	
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
